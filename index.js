47e56274d0ab0e95517c13aabc10ce6c430fd613
Juan Menichetti <jpmenichetti13@gmail.com>
2022-09-17 19:14:43 -0300

    agregamos un puerto del backend

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-const express = require('express');
+// const express = require('express');
+import express from 'express';
 
-console.log("hola mundo");
+// instancia de express
+const app = express();
+
+// queremos tomar un puerto de la pc
+app.set('port', process.env.PORT || 4000);
+
+// quiero que mi backend escuche el puerto creado
+app.listen(app.get('port'), ()=>{
+
+    console.log('Estamos en el puerto ' + app.get('port'));
+})
