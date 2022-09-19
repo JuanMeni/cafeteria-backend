e94e9c0e811ff798d5fadf9e76e879f2a4eb0f2d
Juan Menichetti <jpmenichetti13@gmail.com>
2022-09-19 03:19:10 -0300

    agregamos otra conexion a mongo atlas

--- src/database.js
+++ src/database.js
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 
-const url = 'mongodb://localhost:27017/cafeteriac2i';
+const url = 'mongodb+srv://JuanMeni:37105385zoster@clusterc2i.gn72gyw.mongodb.net/cafec2i'; //db publicada
+// const url = 'mongodb://localhost:27017/cafeteriac2i'; base de dato local
+
 
 mongoose.connect(url);
 
